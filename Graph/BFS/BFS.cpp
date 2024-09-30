8533cf78437b12a9adfb1fb1f7a252ccc434709a
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2024-09-30 17:59:30 +0600

    Create BFS.cpp

---
+++ Graph/BFS/BFS.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//_______________________________________________________________________________________________
+ 
+const int mod = 1e9 + 7;
+#define pi 3.141592653
+#define ll long long int
+#define all(x) x.begin(),x.end()
+#define Yes cout << "Yes\n";
+#define YES cout << "YES\n";
+#define yes cout << "yes\n";
+#define NO cout << "NO\n";
+#define No cout << "No\n";
+#define no cout << "no\n";
+#define _1 cout << -1 << "\n";
+ 
+#define tc ll ttc; cin >> ttc; while(ttc--)
+#define fast_read() (ios_base :: sync_with_stdio(false), cin.tie(NULL));
+ 
+//_______________________________________________________________________________________________
+ 
+ 
+int main(){
+    fast_read();
+    
+    return 0;
+}
+ 
+ 
+//_______________________________________________________________________________________________
