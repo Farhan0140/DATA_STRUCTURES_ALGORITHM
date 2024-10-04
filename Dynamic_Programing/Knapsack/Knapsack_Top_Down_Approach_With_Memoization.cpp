a6139437da07047c0280a92ed50f090fec1177c1
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2024-10-04 23:05:59 +0600

    Update Knapsack_Top_Down_Approach_With_Memoization.cpp

--- Dynamic_Programing/Knapsack/Knapsack_Top_Down_Approach_With_Memoization.cpp
+++ Dynamic_Programing/Knapsack/Knapsack_Top_Down_Approach_With_Memoization.cpp
@@ -12,6 +12,27 @@ using namespace std;
 /*
 https://quanticdev.com/tools/recursion-visualization/
 
+def fn(n, W):
+
+ if(n==0 or W==0): return 0
+
+ 
+
+ if(weight[n-1] <= W):
+
+ op1 = fn(n-1, W-weight[n-1])+value[n-1]
+
+ op2 = fn(n-1, W)
+
+ return max(op1, op2)
+
+ else:
+
+ op2 = fn(n-1, W)
+
+ return op2
+
+
 4
 2 3 4 5
 1 3 5 3
